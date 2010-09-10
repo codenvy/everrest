41416ff662480ab50e04903fec0f07d81562411d
Andrey Parfonov <andrew00x@gmail.com>
2010-09-10 10:01:16 +0000

    EVERREST-2

--- everrest-ext/src/main/java/org/everrest/ext/provider/XSLTStreamingOutput.java
+++ everrest-ext/src/main/java/org/everrest/ext/provider/XSLTStreamingOutput.java
@@ -18,20 +18,14 @@
  */
 package org.everrest.ext.provider;
 
-import org.exoplatform.container.ExoContainer;
-import org.exoplatform.container.ExoContainerContext;
-import org.exoplatform.services.xml.transform.NotSupportedIOTypeException;
-import org.exoplatform.services.xml.transform.impl.trax.TRAXTemplatesServiceImpl;
-import org.exoplatform.services.xml.transform.trax.TRAXTemplates;
-import org.exoplatform.services.xml.transform.trax.TRAXTemplatesService;
-import org.exoplatform.services.xml.transform.trax.TRAXTransformer;
-
 import java.io.IOException;
 import java.io.OutputStream;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.StreamingOutput;
 import javax.xml.transform.Source;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamResult;
@@ -39,7 +33,7 @@ import javax.xml.transform.stream.StreamResult;
 /**
  * This type should be used by resource methods when need to apply XSLT
  * transformation for returned {@link Source}.
- * 
+ *
  * @see StreamingOutput
  * @author <a href="dkatayev@gmail.com">Dmytro Katayev</a>
  * @version $Id: XLSTStreamingOutPut.java
@@ -47,69 +41,39 @@ import javax.xml.transform.stream.StreamResult;
 public class XSLTStreamingOutput implements StreamingOutput
 {
 
-   private String schemeName;
-
    private Source source;
+
+   private Templates templates;
 
    /**
     * XSLTStreamingOutput constructor.
-    * 
-    * @param schemeName XLST scheme name. Must be registered in
-    *        {@link org.exoplatform.services.xml.transform.impl.trax.TRAXTemplatesLoaderPlugin
-    *        TRAXTemplatesLoaderPlugin }
+    *
     * @param source entity to write into output stream.
+    * @param templates transformation templates
     */
-   public XSLTStreamingOutput(String schemeName, Source source)
+   public XSLTStreamingOutput(Source source, Templates templates)
    {
-
-      this.schemeName = schemeName;
       this.source = source;
-
+      this.templates = templates;
    }
 
    /**
     * {@inheritDoc} .
     */
-   public void write(OutputStream outStream) throws IOException, WebApplicationException
+   public void write(OutputStream output) throws IOException, WebApplicationException
    {
-
-      ExoContainer container = ExoContainerContext.getCurrentContainer();
-
-      TRAXTemplatesService templatesService =
-         (TRAXTemplatesService)container.getComponentInstanceOfType(TRAXTemplatesServiceImpl.class);
-
       try
       {
-         TRAXTransformer transformer = null;
-         if (schemeName != null)
-         {
-            TRAXTemplates t = templatesService.getTemplates(schemeName);
-            if (t == null)
-            {
-               String msg = "Template " + schemeName + " not found.";
-               throw new IllegalArgumentException(msg);
-            }
-            transformer = t.newTransformer();
-         }
-         else
-         {
-            String msg = "XSLT scheme name is null.";
-            throw new NullPointerException(msg);
-         }
-         transformer.initResult(new StreamResult(outStream));
-         transformer.transform(source);
+         Transformer transformer = templates.newTransformer();
+         transformer.transform(source, new StreamResult(output));
       }
       catch (TransformerConfigurationException tce)
       {
-         throw new IOException("Can't write to output stream " + tce);
-      }
-      catch (NotSupportedIOTypeException nse)
-      {
-         throw new IOException("Can't write to output stream " + nse);
+         throw new IOException(tce.getMessage(), tce);
       }
       catch (TransformerException tre)
       {
-         throw new IOException("Can't write to output stream " + tre);
+         throw new IOException(tre.getMessage(), tre);
       }
    }
 
---
+++ everrest-ext/src/test/java/org/everrest/ext/provider/XSLTTransformationTest.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.everrest.ext.provider;
+
+import junit.framework.TestCase;
+
+import org.everrest.core.impl.ApplicationPublisher;
+import org.everrest.core.impl.ContainerResponse;
+import org.everrest.core.impl.ProviderBinder;
+import org.everrest.core.impl.RequestHandlerImpl;
+import org.everrest.core.impl.ResourceBinderImpl;
+import org.everrest.core.impl.SimpleDependencySupplier;
+import org.everrest.core.tools.ByteArrayContainerResponseWriter;
+import org.everrest.core.tools.ResourceLauncher;
+import org.exoplatform.services.log.LogConfigurator;
+import org.exoplatform.services.log.impl.Log4JConfigurator;
+
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.StreamingOutput;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Providers;
+import javax.xml.transform.Templates;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ * @author <a href="mailto:andrew00x@gmail.com">Andrey Parfonov</a>
+ * @version $Id$
+ */
+public class XSLTTransformationTest extends TestCase
+{
+
+   public static class Application0 extends javax.ws.rs.core.Application
+   {
+      private final Set<Class<?>> classes = new HashSet<Class<?>>();
+
+      private final Set<Object> objects = new HashSet<Object>();
+
+      public Application0() throws Exception
+      {
+         XSLTTemplatesContextResolver resolver = new XSLTTemplatesContextResolver();
+         resolver.addAsTemplate("test.template", new StreamSource(Thread.currentThread().getContextClassLoader()
+            .getResourceAsStream("xslt/book.xsl")));
+         objects.add(resolver);
+         classes.add(Resource0.class);
+      }
+
+      @Override
+      public Set<Class<?>> getClasses()
+      {
+         return classes;
+      }
+
+      @Override
+      public Set<Object> getSingletons()
+      {
+         return objects;
+      }
+   }
+
+   @Path("a")
+   public static class Resource0
+   {
+      @Context
+      private Providers providers;
+
+      @GET
+      public StreamingOutput m0()
+      {
+         ContextResolver<XSLTTemplatesContextResolver> resolverWrapper =
+            providers.getContextResolver(XSLTTemplatesContextResolver.class, MediaType.APPLICATION_XML_TYPE);
+         XSLTTemplatesContextResolver resolver = resolverWrapper.getContext(null);
+         Templates templates = resolver.getTemplates("test.template");
+         return new XSLTStreamingOutput(new StreamSource(Thread.currentThread().getContextClassLoader()
+            .getResourceAsStream("book-in.xml")), templates);
+      }
+   }
+
+   protected ProviderBinder providers;
+
+   protected ResourceBinderImpl resources;
+
+   protected RequestHandlerImpl requestHandler;
+
+   private ResourceLauncher launcher;
+
+   public void setUp() throws Exception
+   {
+      LogConfigurator lc = new Log4JConfigurator();
+      Properties props = new Properties();
+      props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("conf/log4j.properties"));
+      lc.configure(props);
+
+      this.resources = new ResourceBinderImpl();
+      this.requestHandler = new RequestHandlerImpl(resources, new SimpleDependencySupplier());
+      this.providers = ProviderBinder.getInstance();
+      this.launcher = new ResourceLauncher(requestHandler);
+   }
+
+   public void testTransformToHtml() throws Exception
+   {
+      ApplicationPublisher deployer = new ApplicationPublisher(resources, providers);
+      deployer.publish(new Application0());
+      ByteArrayContainerResponseWriter writer = new ByteArrayContainerResponseWriter();
+      ContainerResponse response = launcher.service("GET", "/a", "", null, null, writer, null);
+      assertEquals(200, response.getStatus());
+      System.out.println(new String(writer.getBody()));
+   }
+
+}
