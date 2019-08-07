7796d68c94575425a753c96553f96d0a85f78f88
Sergii Kabashniuk <skabashniuk@redhat.com>
2019-08-07 09:59:44 +0200

    Migrate to EPL 2

--- everrest-core/src/test/java/org/everrest/core/impl/uri/PathSegmentImplTest.java
+++ everrest-core/src/test/java/org/everrest/core/impl/uri/PathSegmentImplTest.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2012-2016 Codenvy, S.A.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ */
 package org.everrest.core.impl.uri;
 
 import org.junit.Test;
