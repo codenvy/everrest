2d668174bc478749ad1ba99f3ea19c813f3b74c7
Sergii Kabashniuk <skabashniuk@redhat.com>
2021-07-30 15:33:07 +0300

    Update license configuration

--- everrest-groovy/src/test/resources/repo/a/b/GResource1.groovy
+++ everrest-groovy/src/test/resources/repo/a/b/GResource1.groovy
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012-2016 Codenvy, S.A.
+ * Copyright (c) 2012-2021 Codenvy, S.A.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
